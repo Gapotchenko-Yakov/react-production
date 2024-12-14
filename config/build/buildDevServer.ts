cecdb56da7029a02bfddb4fbc716d1ec7d025901
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-12-14 16:40:19 +0300

    Add routing. Add historyApiFallback option ro dev server config.

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -2,5 +2,9 @@ import { BuildOptions } from "./types/config";
 import { Configuration as DevServerConfiguration } from "webpack-dev-server";
 
 export function buildDevServer({ port }: BuildOptions): DevServerConfiguration {
-  return { port, open: true };
+  return {
+    port,
+    open: true,
+    historyApiFallback: true,
+  };
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
+import { Route, Routes } from "react-router-dom";
 import Counter from "./components/Counter";
 import "./index.scss";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import MainPage from "./pages/MainPage/MainPage";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
-      <h2>App</h2>
-      <Counter />
+      <Link to="/">Главная</Link>
+      <Link to="/about">О сайте</Link>
+      <Routes>
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<MainPage />} />
+      </Routes>
     </div>
   );
 };
