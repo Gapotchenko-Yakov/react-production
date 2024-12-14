38648bed49f429b2b6e4474ac0f2b88d53f64cee
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-12-14 16:55:30 +0300

    Using lazy and Suspense for code splitting.

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
 import { Route, Routes } from "react-router-dom";
-import Counter from "./components/Counter";
 import "./index.scss";
-import AboutPage from "./pages/AboutPage/AboutPage";
-import MainPage from "./pages/MainPage/MainPage";
 import { Link } from "react-router-dom";
+import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
+import { MainPageAsync } from "./pages/MainPage/MainPage.async";
+import { Suspense } from "react";
 
 const App = () => {
   return (
     <div className="app">
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
-      <Routes>
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/" element={<MainPage />} />
-      </Routes>
+
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/about" element={<AboutPageAsync />} />
+          <Route path="/" element={<MainPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
