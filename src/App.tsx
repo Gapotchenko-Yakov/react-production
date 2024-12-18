1e660c201ba596dc4e80e4254ab3f2a7338f3244
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-12-18 21:47:37 +0300

    Add theme toggle in the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,24 @@ import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
+
+export enum Theme {
+  LIGHT = "light",
+  DARK = "dark",
+}
 
 const App = () => {
+  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
+
+  const toggleTheme = () =>
+    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+
   return (
-    <div className="app dark">
+    <div className={`app ${theme}`}>
       <Link to="/">Главная</Link>
       <Link to="/about">О сайте</Link>
+      <button onClick={toggleTheme}>Изменить тему</button>
 
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
