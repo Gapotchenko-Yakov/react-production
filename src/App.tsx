23cc4fec0d6df7e0eb2f906e8c1eaf27c9999653
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-12-18 22:39:28 +0300

    Using context for app theme.

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,11 @@ import "./styles/index.scss";
 import { Link } from "react-router-dom";
 import { AboutPageAsync } from "./pages/AboutPage/AboutPage.async";
 import { MainPageAsync } from "./pages/MainPage/MainPage.async";
-import { Suspense, useState } from "react";
-
-export enum Theme {
-  LIGHT = "light",
-  DARK = "dark",
-}
+import { Suspense } from "react";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
-  const [theme, setTheme] = useState<Theme>(Theme.LIGHT);
-
-  const toggleTheme = () =>
-    setTheme(theme === Theme.DARK ? Theme.LIGHT : Theme.DARK);
+  const { theme, toggleTheme } = useTheme();
 
   return (
     <div className={`app ${theme}`}>
