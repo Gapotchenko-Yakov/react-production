5f84ec9fb1cfea85773001679c9f4f1784d90c39
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-12-29 14:38:31 +0300

    Write tests for Button.

--- src/shared/ui/Button/Button.test.tsx
+++ src/shared/ui/Button/Button.test.tsx
@@ -1,10 +1,28 @@
 import { screen, render } from '@testing-library/react';
-import Button from 'shared/ui/Button/Button';
+import Button, { ButtonTheme } from 'shared/ui/Button/Button';
 
 describe('Button tests', () => {
     test('Button renders', () => {
         render(<Button> Test Button </Button>);
         const btnElement = screen.getByRole('button');
         expect(btnElement).toBeInTheDocument();
+    });
+
+    test('Button renders with class', () => {
+        render(<Button> Test Button </Button>);
+        const btnElement = screen.getByRole('button');
+        expect(btnElement).toHaveClass('Button');
+    });
+
+    test('Button renders without very specific class', () => {
+        render(<Button> Test Button </Button>);
+        const btnElement = screen.getByRole('button');
+        expect(btnElement).not.toHaveClass('some_very_spec_class_3234');
+    });
+
+    test('Button renders with clear theme', () => {
+        render(<Button theme={ButtonTheme.CLEAR}> Test Button </Button>);
+        const btnElement = screen.getByRole('button');
+        expect(btnElement).toHaveClass('clear');
     });
 });
