32ba2d6135976d6cb855a6d6702197216a30be00
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-12-29 13:31:47 +0300

    Add BundleAnalyzerPlugin for webpack. Change max len rule for eslint.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,8 +1,8 @@
-import path from 'path';
 import webpack, { WebpackPluginInstance } from 'webpack';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
+import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({
@@ -20,6 +20,9 @@ export function buildPlugins({
         }),
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
+        }),
+        new BundleAnalyzerPlugin({
+            openAnalyzer: false,
         }),
     ];
 
