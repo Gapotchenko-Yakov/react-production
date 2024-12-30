be31c7d326160c486208b0279157a61e1192946d
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2024-12-30 18:40:16 +0300

    Prepare for ci config: add scripts to package.json, fix eslint errors.

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,26 +1,31 @@
 import { classNames } from 'shared/lib';
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?: string;
 }
 
-const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink
-                to="/"
-                className={cls.mainLink}
-                theme={AppLinkTheme.PRIMARY}
-            >
-                Главная
-            </AppLink>
-            <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
-                О сайте
-            </AppLink>
+const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink
+                    to="/"
+                    className={cls.mainLink}
+                    theme={AppLinkTheme.PRIMARY}
+                >
+                    {t('Main')}
+                </AppLink>
+                <AppLink to="/about" theme={AppLinkTheme.SECONDARY}>
+                    {t('About')}
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Navbar;
--- src/widgets/Sidebar/ui/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar.tsx
@@ -3,6 +3,7 @@ import { classNames } from 'shared/lib';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
 import { Button } from 'shared/ui';
+import { useTranslation } from 'react-i18next';
 import cls from './Sidebar.module.scss';
 
 interface SidebarProps {
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 const Sidebar: FC<SidebarProps> = ({ className }) => {
     const [collapsed, setCollapsed] = useState(false);
+    const { t } = useTranslation();
 
     const onToggle = () => {
         setCollapsed((prev) => !prev);
@@ -23,7 +25,7 @@ const Sidebar: FC<SidebarProps> = ({ className }) => {
             ])}
             data-testid="sidebar"
         >
-            <Button onClick={onToggle}>Toggle</Button>
+            <Button onClick={onToggle}>{t('Toggle')}</Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
                 <LangSwitcher className={cls.lang} />
