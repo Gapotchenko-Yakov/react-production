cdd63a20024bac669b33e1e4f1d10b4542239b1e
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-02 11:57:14 +0300

    Remove Modal from App. Using Modal in the the Navbar.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,28 +1,18 @@
-import { Suspense, useState } from 'react';
+import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import Loader from 'shared/ui/Loader/Loader';
-import Modal from 'shared/ui/Modal/Modal';
-import Button from 'shared/ui/Button/Button';
 
 function App() {
     const { theme } = useTheme();
-    const [isOpen, setIsOpen] = useState(false);
 
     return (
         <div className={classNames('app', {}, [theme])}>
             <Suspense fallback={<Loader />}>
                 <Navbar />
-                <Button onClick={() => setIsOpen(true)}>Modal Toggle</Button>
-                <Modal isOpen={isOpen} onClose={() => setIsOpen(false)}>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                    Facilis id dolorem vitae perspiciatis labore ut,
-                    at illo odit nostrum itaque inventore culpa maxime,
-                    similique incidunt eveniet minus libero, quo beatae?
-                </Modal>
                 <div className="content-page">
                     <Sidebar />
                     <AppRouter />
