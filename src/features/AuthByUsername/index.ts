1797caa44e272164c92fad623b6d7713766a6910
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-03 21:21:22 +0300

    Install deps. Add model segment for the AuthByUsername feature. Simplify pre commit husky checks.

--- src/features/AuthByUsername/index.ts
+++ src/features/AuthByUsername/index.ts
@@ -1,3 +1,12 @@
 export {
     LoginModal,
+
 } from './ui/LoginModal/LoginModal';
+
+export {
+    LoginSchema,
+} from './model/types/loginSchema';
+
+export {
+    loginReducer,
+} from './model/slice/loginSlice';
