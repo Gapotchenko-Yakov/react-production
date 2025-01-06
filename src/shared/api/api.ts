aabeeeda330bee313c4532d540cd71e6cbc82fb7
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-06 21:24:42 +0300

    Add global __API__ and use it in the api instance.

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -1,10 +1,8 @@
 import axios from 'axios';
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage';
 
-const baseUrl = 'http://localhost:8000';
-
 export const $api = axios.create({
-    baseURL: baseUrl,
+    baseURL: __API__,
     headers: {
         authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || '',
     },
