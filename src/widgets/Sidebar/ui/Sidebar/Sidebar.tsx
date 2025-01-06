b6cc83684b6b6b03e57da2b87744f340857e0922
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-06 18:49:16 +0300

    Use memo hoc to avoid component rerendering.

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -1,12 +1,8 @@
-import { FC, useState } from 'react';
+import { FC, useCallback, useState } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { ThemeSwitcher } from 'widgets/ThemeSwitcher';
 import { LangSwitcher } from 'widgets/LangSwitcher';
-import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
-import { useTranslation } from 'react-i18next';
 import { Button, ButtonSize, ButtonTheme } from 'shared/ui/Button/Button';
-import { routePath } from 'shared/config/routeConfig/routeConfig';
-
 import { SidebarItems } from 'widgets/Sidebar/model/items';
 import cls from './Sidebar.module.scss';
 import { SidebarItem } from '../SidebarItem/SidebarItem';
@@ -17,11 +13,10 @@ interface SidebarProps {
 
 const Sidebar: FC<SidebarProps> = ({ className }) => {
     const [collapsed, setCollapsed] = useState(false);
-    const { t } = useTranslation();
 
-    const onToggle = () => {
+    const onToggle = useCallback(() => {
         setCollapsed((prev) => !prev);
-    };
+    }, []);
 
     return (
         <div
@@ -33,7 +28,7 @@ const Sidebar: FC<SidebarProps> = ({ className }) => {
             <Button
                 data-testid="toggle"
                 onClick={onToggle}
-                className={classNames(cls.collapseBtn)}
+                className={cls.collapseBtn}
                 theme={ButtonTheme.BACKGROUND_INVERTED}
                 square
                 size={ButtonSize.L}
@@ -47,7 +42,6 @@ const Sidebar: FC<SidebarProps> = ({ className }) => {
                         <SidebarItem item={item} collapsed={collapsed} />
                     ))
                 }
-
             </div>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
--- src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
+++ src/widgets/Sidebar/ui/SidebarItem/SidebarItem.tsx
@@ -2,6 +2,7 @@ import { classNames } from 'shared/lib/classNames/classNames';
 import AppLink, { AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import { SidebarItemType } from 'widgets/Sidebar/model/items';
 import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
 import cls from './SidebarItem.module.scss';
 
 interface SidebarItemProps {
@@ -9,7 +10,7 @@ item: SidebarItemType;
 collapsed: boolean
 }
 
-export const SidebarItem = (props: SidebarItemProps) => {
+export const SidebarItem = memo((props: SidebarItemProps) => {
     const { item, collapsed } = props;
     const { t } = useTranslation();
 
@@ -25,4 +26,4 @@ export const SidebarItem = (props: SidebarItemProps) => {
             </span>
         </AppLink>
     );
-};
+});
