2266d6969988fa35d0a13cbeb2adb8c75f613c86
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-07 12:41:15 +0300

    Add fetchProfileData service.

--- src/entities/Profile/model/slice/profileSlice.ts
+++ src/entities/Profile/model/slice/profileSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { ProfileSchema } from '../types/profile';
+import { fetchProfileData } from '../services/fetchProfileData/fetchProfileData';
 
 const initialState: ProfileSchema = {
     data: undefined,
@@ -12,6 +13,24 @@ export const profileSlice = createSlice({
     name: 'profile',
     initialState,
     reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProfileData.pending, (state) => {
+                state.error = undefined;
+                state.data = undefined;
+                state.isLoading = true;
+            })
+            .addCase(fetchProfileData.rejected, (state, action) => {
+                state.error = action.payload;
+                state.data = undefined;
+                state.isLoading = false;
+            })
+            .addCase(fetchProfileData.fulfilled, (state, action) => {
+                state.error = undefined;
+                state.data = action.payload;
+                state.isLoading = false;
+            });
+    },
 });
 
 export const { actions: profileActions } = profileSlice;
