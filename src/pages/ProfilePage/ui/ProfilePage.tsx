407867aa68fc54751120a1459102a7d6c1433dc2
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-07 13:07:10 +0300

    Add fetchProfileData async thunk and ProfileCard.

--- src/pages/ProfilePage/ui/ProfilePage.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.tsx
@@ -1,9 +1,15 @@
-import { profileReducer } from 'entities/Profile';
+import {
+    fetchProfileData,
+    profileReducer,
+    ProfileCard,
+} from 'entities/Profile';
+import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
     DynamicModuleLoader,
     ReducersList,
 } from 'shared/lib/components/DynamicModuleLoader/DynamicModuleLoader';
+import { useAppDispatch } from 'shared/lib/hooks/useAppDispatch/useAppDispatch';
 
 interface ProfilePageProps {}
 
@@ -14,14 +20,18 @@ const reducersList: ReducersList = {
 const ProfilePage = (props: ProfilePageProps) => {
     const { t } = useTranslation();
 
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProfileData());
+    }, [dispatch]);
+
     return (
         <DynamicModuleLoader
             reducers={reducersList}
             removeAfterUnmount
         >
-            <div>
-                {t('PROFILE PAGE')}
-            </div>
+            <ProfileCard />
         </DynamicModuleLoader>
     );
 };
