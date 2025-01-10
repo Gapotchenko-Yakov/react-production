e5c9c9fac4a7e2a9d4e9297fe8d4ae882097bf4f
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-10 17:26:11 +0300

    Fix stories for ProfilePage and ProfileCard.

---
+++ src/entities/Profile/ui/ProfileCard/ProfileCard.stories.tsx
@@ -0,0 +1,49 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+import AvatarImg from 'shared/assets/tests/storybook.jpg';
+import { ProfileCard } from './ProfileCard';
+
+export default {
+    title: 'entities/ProfileCard',
+    component: ProfileCard,
+    argTypes: {
+        backgroundColor: { control: 'color' },
+    },
+} as ComponentMeta<typeof ProfileCard>;
+
+const Template: ComponentStory<typeof ProfileCard> = (args) => <ProfileCard {...args} />;
+
+export const Light = Template.bind({});
+Light.args = {
+    data: {
+        first: 'Иван',
+        lastname: 'Петров',
+        age: 27,
+        currency: Currency.USD,
+        country: Country.Belarus,
+        city: 'Moscow',
+        username: 'admin12',
+        avatar: AvatarImg,
+    },
+};
+Light.decorators = [];
+
+export const Dark = Template.bind({});
+Dark.args = {
+    data: {
+        first: 'Иван',
+        lastname: 'Петров',
+        age: 27,
+        currency: Currency.USD,
+        country: Country.Belarus,
+        city: 'Moscow',
+        username: 'admin12',
+        avatar: AvatarImg,
+    },
+};
+Dark.decorators = [
+    ThemeDecorator(Theme.DARK),
+];
--- src/pages/ProfilePage/ui/ProfilePage.stories.tsx
+++ src/pages/ProfilePage/ui/ProfilePage.stories.tsx
@@ -2,6 +2,9 @@ import { ComponentMeta, ComponentStory } from '@storybook/react';
 import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { Theme } from 'app/providers/ThemeProvider';
 import { StoreDecorator } from 'shared/config/storybook/StoreDecorator/StoreDecorator';
+import { Currency } from 'entities/Currency';
+import { Country } from 'entities/Country';
+import AvatarImg from 'shared/assets/tests/storybook.jpg';
 import ProfilePage from './ProfilePage';
 
 export default {
@@ -16,8 +19,39 @@ const Template: ComponentStory<typeof ProfilePage> = (args) => <ProfilePage {...
 
 export const Light = Template.bind({});
 Light.args = {};
-Light.decorators = [StoreDecorator({})];
+Light.decorators = [
+    StoreDecorator({
+        profile: {
+            form: {
+                first: 'Иван',
+                lastname: 'Петров',
+                age: 27,
+                currency: Currency.USD,
+                country: Country.Belarus,
+                city: 'Moscow',
+                username: 'admin12',
+                avatar: AvatarImg,
+            },
+        },
+    }),
+];
 
 export const Dark = Template.bind({});
 Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({})];
+Dark.decorators = [
+    ThemeDecorator(Theme.DARK),
+    StoreDecorator({
+        profile: {
+            form: {
+                first: 'Иван',
+                lastname: 'Петров',
+                age: 27,
+                currency: Currency.USD,
+                country: Country.Belarus,
+                city: 'Moscow',
+                username: 'admin12',
+                avatar: AvatarImg,
+            },
+        },
+    }),
+];
