6fa9a1aee62ac842c347b4b60b742febfedb39cb
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-11 22:06:13 +0300

    Config react refresh plugin.

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -26,6 +26,7 @@ export function buildPlugins({
 
     if (isDev) {
         plugins.push(new webpack.HotModuleReplacementPlugin());
+        plugins.push(new webpack.HotModuleReplacementPlugin());
         plugins.push(new BundleAnalyzerPlugin({
             openAnalyzer: false,
         }));
