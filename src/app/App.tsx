5ee29112c1f483c297594c16e82f2b4284100d42
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-12 14:06:45 +0300

    Render AppRouter only if user inited.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,13 +5,14 @@ import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
 import Loader from 'shared/ui/Loader/Loader';
-import { useDispatch } from 'react-redux';
-import { userActions } from 'entities/User';
+import { useDispatch, useSelector } from 'react-redux';
+import { getUserInited, userActions } from 'entities/User';
 
 function App() {
     const { theme } = useTheme();
 
     const dispatch = useDispatch();
+    const inited = useSelector(getUserInited);
 
     useEffect(() => {
         dispatch(userActions.initAuthData());
@@ -23,7 +24,7 @@ function App() {
                 <Navbar />
                 <div className="content-page">
                     <Sidebar />
-                    <AppRouter />
+                    {inited && <AppRouter />}
                 </div>
             </Suspense>
         </div>
