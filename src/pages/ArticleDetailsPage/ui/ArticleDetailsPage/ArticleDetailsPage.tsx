3e3cda7b4b038e3a6b55d3306aa552134672292f
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-12 15:03:57 +0300

    Delete style files.

--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
-import cls from './ArticleDetailsPage.module.scss';
 
 interface ArticleDetailsPageProps {
     className?: string;
@@ -13,7 +12,7 @@ const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
 
     return (
         // eslint-disable-next-line i18next/no-literal-string
-        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+        <div className={classNames('', {}, [className])}>
             ARTICLE DETAILS
         </div>
     );
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.tsx
@@ -1,7 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTranslation } from 'react-i18next';
 import { memo } from 'react';
-import cls from './ArticlesPage.module.scss';
 
 interface ArticlesPageProps {
     className?: string;
@@ -13,7 +12,7 @@ const ArticlesPage = (props: ArticlesPageProps) => {
 
     return (
         // eslint-disable-next-line i18next/no-literal-string
-        <div className={classNames(cls.ArticlesPage, {}, [className])}>
+        <div className={classNames('', {}, [className])}>
             ARTICLES PAGE
         </div>
     );
