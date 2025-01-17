e0bbb353b362dd233a06b0a36fcc1174f8b32dfb
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-01-17 23:49:10 +0300

    Create Article List and Article List Item components.

---
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -0,0 +1,41 @@
+import { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Article, ArticleView } from 'entities/Article/model/types/article';
+import cls from './ArticleList.module.scss';
+import { ArticleListItem } from '../ArticleListItem/ArticleListItem';
+
+interface ArticleListProps {
+  className?: string;
+  articles: Article[];
+  isLoading: boolean;
+  view: ArticleView;
+}
+
+export const ArticleList = memo((props: ArticleListProps) => {
+    const {
+        className,
+        articles,
+        isLoading,
+        view = ArticleView.SMALL,
+    } = props;
+
+    return (
+        <div className={classNames(cls.ArticleList, {}, [className, cls[view]])}>
+            {
+
+                articles.length
+                    ? articles.map((
+                        article,
+                    ) => (
+                        <ArticleListItem
+                            className={cls.card}
+                            key={article.id}
+                            view={view}
+                            article={article}
+                        />
+                    ))
+                    : null
+            }
+        </div>
+    );
+});
