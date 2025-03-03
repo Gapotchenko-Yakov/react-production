57086ee3d20628b0465555c7afe7f6116fdf5894
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-03-03 16:58:46 +0300

    Add rtkApi and use it for articles recommendations list.

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
@@ -1,17 +1,12 @@
-import { classNames } from 'shared/lib/classNames/classNames';
-import { useTranslation } from 'react-i18next';
-import { memo } from 'react';
-import { VStack } from 'shared/ui/Stack';
 import { ArticleList } from 'entities/Article';
+import { memo } from 'react';
+import { useTranslation } from 'react-i18next';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { VStack } from 'shared/ui/Stack';
 import { Text, TextSize } from 'shared/ui/Text/Text';
-import { useInitialEffect } from 'shared/lib/hooks/useInitialEffect/useInitialEffect';
-import { useDispatch, useSelector } from 'react-redux';
 import {
-    fetchArticleRecommendations,
-} from 'pages/ArticleDetailsPage/model/services/fetchArticleRecommendations/fetchArticleRecommendations';
-import { getArticleRecommendationsIsLoading } from 'pages/ArticleDetailsPage/model/selectors/recommendations';
-import { getArticleRecommendations } from 'pages/ArticleDetailsPage/model/slice/articleDetailsPageRecommendationsSlice';
-import cls from './ArticleRecommendationsList.module.scss';
+    useGetArticleRecommendationsListQuery,
+} from '../../api/articleRecommendationsApi';
 
 interface ArticleRecommendationsListProps {
     className?: string;
@@ -21,26 +16,20 @@ export const ArticleRecommendationsList = memo((props: ArticleRecommendationsLis
     const { className } = props;
     const { t } = useTranslation();
 
-    const dispatch = useDispatch();
+    const { isLoading, data: articles, error } = useGetArticleRecommendationsListQuery(3);
 
-    const recommendations = useSelector(getArticleRecommendations.selectAll);
-    const recommendationsIsLoading = useSelector(getArticleRecommendationsIsLoading);
-
-    useInitialEffect(() => {
-        dispatch(fetchArticleRecommendations());
-    });
+    if (isLoading || error) {
+        return null;
+    }
 
     return (
         <VStack gap="8" className={classNames('', {}, [className])}>
             <Text
                 size={TextSize.L}
-                className={cls.commentTitle}
                 title={t('Рекомендуем')}
             />
             <ArticleList
-                articles={recommendations}
-                isLoading={recommendationsIsLoading}
-                className={cls.recommendations}
+                articles={articles}
                 target="_blank"
             />
         </VStack>
