97da43d30c604c37b3d1751afae713f9e3424fb1
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-03-05 12:51:58 +0300

    Add jest-html-reporters npm package for unit tests.

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -42,6 +42,15 @@ export default {
         '\\.s?css$': 'identity-obj-proxy',
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
@@ -106,9 +115,6 @@ export default {
 
     // Run tests from one or more projects
     // projects: undefined,
-
-    // Use this configuration option to add custom reporters to Jest
-    // reporters: undefined,
 
     // Automatically reset mock state before every test
     // resetMocks: false,
