83124f0c825c69cd345a29b6933da0d4ce77efd5
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-03-05 13:14:27 +0300

    Fix: article recommendations not displayed after reopening article details page.

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
@@ -31,6 +31,7 @@ export const ArticleRecommendationsList = memo((props: ArticleRecommendationsLis
             <ArticleList
                 articles={articles}
                 target="_blank"
+                virtualized={false}
             />
         </VStack>
     );
