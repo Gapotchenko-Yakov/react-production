fd390855c7c847f89d13239eab6def134a076a24
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-03-15 13:29:03 +0300

    Fix stroybook absolute imports.

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.stories.tsx
@@ -1,6 +1,7 @@
 import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import { AvatarDropdown } from './AvatarDropdown';
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 
 export default {
     title: 'features/AvatarDropdown',
@@ -16,3 +17,6 @@ export const Normal = Template.bind({});
 Normal.args = {
 
 };
+Normal.decorators = [
+    StoreDecorator({}),
+];
