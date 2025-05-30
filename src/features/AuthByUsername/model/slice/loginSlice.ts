1169973110c45f4a544137d3e79b7e5f3aa547dc
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-05-30 19:54:06 +0300

    feat: add ArticlePageGreeting feature

--- src/features/AuthByUsername/model/slice/loginSlice.ts
+++ src/features/AuthByUsername/model/slice/loginSlice.ts
@@ -26,7 +26,6 @@ export const loginSlice = createSlice({
                 state.isLoading = true;
             })
             .addCase(loginByUsername.fulfilled, (state) => {
-                state.error = undefined;
                 state.isLoading = false;
             })
             .addCase(loginByUsername.rejected, (state, action) => {
