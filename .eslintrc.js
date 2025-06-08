ced1a40693fe451e851eb1404e4499312c2641ee
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-06-08 19:20:59 +0300

    fix(ArticlesPage): adjust search input size

--- .eslintrc.js
+++ .eslintrc.js
@@ -64,6 +64,7 @@ module.exports = {
                     'feature',
                     'variant',
                     'color',
+                    'size',
                 ],
             },
         ],
