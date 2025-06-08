6b4329c75d97338e4a16962041358e9b4531f5ec
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-06-08 23:51:50 +0300

    feat(Article): redesign ArticleList component

--- .eslintrc.js
+++ .eslintrc.js
@@ -65,6 +65,7 @@ module.exports = {
                     'variant',
                     'color',
                     'size',
+                    'wrap',
                 ],
             },
         ],
